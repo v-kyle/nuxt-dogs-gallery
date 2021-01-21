56688b89673b9ab8374596bf34e7096ce3202a01
Leonid Nikulin <leonk2020@gmail.com>
2021-01-21 17:01:01 +0300

    delete useless code

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxt-dogs-gallery</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1 class="title">nuxt-dogs-gallery</h1>
   </div>
 </template>
 
@@ -38,7 +17,7 @@ export default Vue.extend({});
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  /*text-align: center;*/
 }
 
 .title {
